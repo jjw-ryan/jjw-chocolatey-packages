2cf0522f51b86e02383a8a278214e129ffa534fd
jjw-ryan <ryan@jjwilsonltd.com>
2026-07-17 10:19:43 +0100

    AU: 1 updated - victoria-erp-client [skip ci]

--- automatic/victoria-erp-client/tools/chocolateyinstall.ps1
+++ automatic/victoria-erp-client/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop' # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileLocation = Join-Path $toolsDir 'Victoria ERP Client Installer.exe'
-$checksum = 'B7917F64D41C6530D224FB27B9B9A2FB79E97F5B9F4817B560046478FE9DA4D6'
+$checksum = '407098AD3DB1A0B8331221A120A190760E812B44D40475256DC3A66502687D90'
 
 #$chocoLogDir = Join-Path $env:ChocolateyInstall 'logs'
 $innoLog = Join-Path $env:ChocolateyInstall "logs\$($env:ChocolateyPackageName)-install.log"
